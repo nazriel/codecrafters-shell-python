672433eac7638887be5253b9d121fc8cc37757b0
Damian Ziemba <nazriel@dzfl.pl>
2024-06-25 02:38:48 +0200

    Your commit is writing checks your merge can't cash.
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 import sys
 
 
-def main():
+def handle_input(input):
+    if input.strip() == "":
+        return
+
+    command = input.split(" ")
+    sys.stdout.write(f"{command[0]}: command not found\n")
+
+
+def print_prompt():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    # Wait for user input
-    input()
+
+def main():
+    while True:
+        print_prompt()
+        last_input = input()
+        if last_input == "exit":
+            break
+
+        handle_input(last_input)
 
 
 if __name__ == "__main__":
